770a39e4b7827e03588e348c3833de6a16b7a9f7
xkrozer <52842324+xkrozer@users.noreply.github.com>
2019-07-19 13:23:55 -0500

    Update Login.java

--- PuntoVenta Code/src/interfaces/Login.java
+++ PuntoVenta Code/src/interfaces/Login.java
@@ -38,7 +38,7 @@ public class Login extends JFrame /*implements ActionListener,MouseListener*/{
 
     public static final String URL = "jdbc:postgresql://localhost:5432/PuntoVentaIng";
     public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "1234";
+    public static final String PASSWORD = "123456789";
 
     PreparedStatement ps;
     ResultSet rs;
@@ -172,7 +172,7 @@ public class Login extends JFrame /*implements ActionListener,MouseListener*/{
 
         try{
             con = getConection();
-             ps = con.prepareStatement("select * from ingenieria.empleado where usuario = ? and contraseña = ? ");
+             ps = con.prepareStatement("select usuario,contraseña from ingenieria.empleado where usuario = ? and contraseña = ? ");
              // ps.setString(1, );
              ps.setString(1, user.getText());
              ps.setString(2, contrasenna.getText());
